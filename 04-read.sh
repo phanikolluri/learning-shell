22d92ba605305a128579cdf684fdfd4d6471116e
phani <phanendrasai41@gmail.com>
2022-06-19 12:19:25 +0530

    read commit

---
+++ 04-read.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p 'Enter your name: ' name
+echo -e "$name, welcome to devops training"
+
+read -p 'Enter your phone number: ' phone
+echo -e "thanks for entering the details, will text you details soon to $phone"
