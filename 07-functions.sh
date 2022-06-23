0cf78cb8fe615cc7c705c5faa31fde07025794f4
phani <phanendrasai41@gmail.com>
2022-06-23 11:24:52 +0530

    updating ./Learning/learning-shell

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,13 +1,16 @@
 #declare a function
 abc() {
   echo ABC function
+  a=10
+  echo $b
 }
 
 #declare another function
 function xyz() {
   echo XYZ function
 }
-
+echo $a
+b=20
 #acess the function
-xyz
+abc
 
