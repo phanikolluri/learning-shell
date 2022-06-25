546c9f6c65b351674921f39296784793504db34c
phani <phanendrasai41@gmail.com>
2022-06-25 12:29:29 +0530

    updating ./Learning/learning-shell

---
+++ 09-loop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash/
+
+# Fundamental loops : while , for (until & select)
+
+#syntax
+# Loop command expressions or inputs ; do
+    #commands
+# done
+
+#while uses expression
+# uses inputs
+
+i=2
+while [ $i -lt 3]; do
+  echo hello
+  i=$(($i+1))
+done
+
+  
+
